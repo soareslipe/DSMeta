0bf53d0366a00bc4ded79aa5df93f190dd83ef45
unknown <zuraffa2015@gmail.com>
2022-07-14 23:09:40 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository; 
+	
+	@Transactional
+	public List<Sale>findSales() {
+		return repository.findAll();
+	}
+	
+}
